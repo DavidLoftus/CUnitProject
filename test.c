c48ca9c3d57b72a1de8cf5e52e35113e27e7e9ea
Gorzoid <5949829+Gorzoid@users.noreply.github.com>
2019-04-26 12:24:18 +0100

    Large array test

--- test.c
+++ test.c
@@ -7,28 +7,28 @@
 
 #include "avg_and_max.h"
 
-void avg_test1(void)
+void avg_test(void)
 {
     double arr[] = {1.0, 0.0, -1.0};
-    CU_ASSERT_DOUBLE_EQUAL(average(arr, 3), 0.0, 0.01);
-    CU_ASSERT_DOUBLE_EQUAL(average(arr, 2), 0.5, 0.01);
-    CU_ASSERT_DOUBLE_EQUAL(average(arr, 1), 0.5, 0.01);
+    CU_ASSERT_DOUBLE_EQUAL(average(arr, 3), 0.0, 0.0001);
+    CU_ASSERT_DOUBLE_EQUAL(average(arr, 2), 0.5, 0.0001);
+    CU_ASSERT_DOUBLE_EQUAL(average(arr, 1), 1.0, 0.0001);
 }
 
 void avg_test_empty_array(void)
 {
-    double arr[] = {0.0};
-    CU_ASSERT_DOUBLE_EQUAL(average(arr, 0), 0.0, 0.01);
+    CU_ASSERT_DOUBLE_EQUAL(average(NULL, 0), 0.0, 0.0001);
 }
 
 void avg_test_large_array(void)
 {
     double* arr = malloc(1000 * sizeof(double));
-    for(int i = 0; i < 1000; ++arr)
+    CU_ASSERT_PTR_NOT_NULL_FATAL(arr);
+    for(int i = 0; i < 1000; ++i)
     {
         arr[i] = i+1;
     }
-    CU_ASSERT_DOUBLE_EQUAL(average(arr, 1000), 1001/2.0, 0.01);
+    CU_ASSERT_DOUBLE_EQUAL(average(arr, 1000), 1001/2.0, 0.0001);
     free(arr);
 }
 
@@ -38,9 +38,21 @@ void max_test(void)
     CU_ASSERT_EQUAL(max(array, 3), 9.252);
 }
 
-void max_test_empty(void)
+void max_test_empty_array(void)
 {
     CU_ASSERT_EQUAL(max(NULL, 0), -INFINITY);
+}
+
+void max_test_large_array(void)
+{
+    double* arr = malloc(1000 * sizeof(double));
+    CU_ASSERT_PTR_NOT_NULL_FATAL(arr);
+    for(int i = 0; i < 1000; ++i)
+    {
+        arr[i] = i - i*i * 0.01;
+    }
+    CU_ASSERT_DOUBLE_EQUAL(max(arr, 1000), 25.0, 0.0001);
+    free(arr);
 }
 
 int main(void)
@@ -50,12 +62,13 @@ int main(void)
     CU_pSuite avg_suite = CU_add_suite("avg_suite", NULL, NULL);
     CU_pSuite max_suite = CU_add_suite("max_suite", NULL, NULL);
 
-    CU_ADD_TEST(avg_suite, avg_test1);
+    CU_ADD_TEST(avg_suite, avg_test);
     CU_ADD_TEST(avg_suite, avg_test_empty_array);
     CU_ADD_TEST(avg_suite, avg_test_large_array);
 
     CU_ADD_TEST(max_suite, max_test);
-    CU_ADD_TEST(max_suite, max_test_empty);
+    CU_ADD_TEST(max_suite, max_test_empty_array);
+    CU_ADD_TEST(max_suite, max_test_large_array);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
 
